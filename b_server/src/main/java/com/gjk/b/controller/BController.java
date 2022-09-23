0262d2374deb27b4be561d52c0e86c64a79c32ed
gaojiankang <gaojiankang@ykccn.com>
2022-09-23 11:59:38 +0800

    [*]nacos配置中心。测试热更新

--- b_server/src/main/java/com/gjk/b/controller/BController.java
+++ b_server/src/main/java/com/gjk/b/controller/BController.java
@@ -3,6 +3,7 @@ package com.gjk.b.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import com.gjk.b.feign.AClient;
  * @date: 2022/9/19 20:23
  * @description:
  */
+@RefreshScope
 @RestController
 @RequestMapping("/b")
 public class BController {
